fd827500aa548c94449205c5c8ad36e006fe3b09
stanley <stanleykelechi2000@gmail.com>
2025-05-14 14:39:25 +0100

    new stuff

--- app.js
+++ app.js
@@ -15,7 +15,12 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+// const rateLimit = require("express-rate-limit");
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100, // Limit each IP to 100 requests per windowMs
+// });
+// app.use(limiter); // Apply the rate limiting middleware to all requests
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "success",
